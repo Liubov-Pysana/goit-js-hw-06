3643e5532cacdab65f8a39d9cf821af5f7cfb3f3
Liubov-Pysana <144674433+Liubov-Pysana@users.noreply.github.com>
2024-03-28 22:35:55 +0000

    2nd ex done

--- js/task-1.js
+++ js/task-1.js
@@ -1,27 +1,3 @@
-// const customer = {
-//     username: "Mango",
-//     balance: 24000,
-//     discount: 0.1,
-//     orders: ["Burger", "Pizza", "Salad"],
-//     // Change code below this line
-//     getBalance() {
-//         return balance;
-//     },
-//     getDiscount() {
-//         return discount;
-//     },
-//     setDiscount(value) {
-//         discount = value;
-//     },
-//     getOrders() {
-//         return orders;
-//     },
-//     addOrder(cost, order) {
-//         balance -= cost - cost * discount;
-//         orders.push(order);
-//     },
-//     // Change code above this line
-// };
 const customer = {
     username: "Mango",
     balance: 24000,
